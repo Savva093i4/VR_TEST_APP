c7ce9030277e874a2a011d2ac7dd59870e78f311
Savva <savvamolotkov888@gmail.com>
2023-12-13 00:05:30 +0300

    ok

---
+++ Assets/NewBehaviourScript.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+	public void H(Vector2 f)
+	{
+		Debug.Log(f);
+		
+	}
+}
