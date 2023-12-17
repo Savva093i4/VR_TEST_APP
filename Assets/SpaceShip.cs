26138c4c19470ae9fa7e41b7421b69529f45c8c5
Savva <savvamolotkov888@gmail.com>
2023-12-17 06:42:41 +0300

    ok

--- Assets/SpaceShip.cs
+++ Assets/SpaceShip.cs
@@ -10,26 +10,27 @@ public class SpaceShip : MonoBehaviour
 
     [SerializeField] private float MoveSpeed = 100000;
     [SerializeField] private float RotationSpeed = 100000;
-    
-    [SerializeField] private float TorqueSpeed ;
-    
-    
+
+    [SerializeField] private float TorqueSpeed;
+
+    private Vector2 turnDir;
 
 
     private void FixedUpdate()
     {
         rigidbody.AddRelativeTorque(new Vector3(0, LeverVector.x * RotationSpeed, 0), ForceMode.Force);
         rigidbody.AddRelativeForce(new Vector3(0, 0, LeverVector.y * MoveSpeed), ForceMode.Force);
-        
-        
-        var turnDir = LeverVector.normalized;
-        rigidbody.AddTorque(Vector3.up * turnDir.x * TorqueSpeed * -1 *rigidbody.mass);
 
+
+        turnDir = LeverVector.normalized;
+        
+       // rigidbody.AddTorque(Vector3.up * turnDir.x * TorqueSpeed  * rigidbody.mass);
+//!!
         var right = new Vector3(transform.right.x, 0f, transform.right.z);
-        rigidbody.AddTorque(right * turnDir.y * TorqueSpeed  * rigidbody.mass);
+        
+        rigidbody.AddTorque(right * turnDir.y * TorqueSpeed * rigidbody.mass);
+        rigidbody.AddTorque(right * turnDir.x * TorqueSpeed * rigidbody.mass);
     }
 
     public void ChangeAltitude(float percentage) => Debug.Log(percentage);
-    
-    
 }
